70e4a3257dce209f640e1ed64268b85014cebcf4
Astrid Yu <astrid@astrid.tech>
2021-03-26 17:08:39 -0700

    Add docs and tests

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # newbase60-rs
 
-A Rust library that implements [Tantek Celik's New Base 60](http://tantek.pbworks.com/w/page/19402946/NewBase60) number
+This crate includes a safe implementation of [Tantek Celik's New Base 60](http://tantek.pbworks.com/w/page/19402946/NewBase60) number
 system.
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,20 @@
-// see http://tantek.pbworks.com/w/page/19402946/NewBase60
 use rstest::rstest;
 
+/// Convert a number into its New Base 60 representation.
+///
+/// For more information, see [this link](http://tantek.pbworks.com/w/page/19402946/NewBase60).
+///
+/// # Examples
+///
+/// Basic usage:
+/// ```
+/// use newbase60_rs::num_to_sxg;
+///
+/// assert_eq!(num_to_sxg(1337), "NH".to_string());
+/// ```
 pub fn num_to_sxg(n: u128) -> String {
-    static digits: &[u8; 60] = b"0123456789ABCDEFGHJKLMNPQRSTUVWXYZ_abcdefghijkmnopqrstuvwxyz";
+    // A static lookup table
+    static DIGITS: &[u8; 60] = b"0123456789ABCDEFGHJKLMNPQRSTUVWXYZ_abcdefghijkmnopqrstuvwxyz";
 
     if n == 0 {
         return "0".to_string();
@@ -11,14 +23,38 @@ pub fn num_to_sxg(n: u128) -> String {
     let mut n = n;
     let mut s = String::new();
     while n > 0 {
+        // Safe to convert because it is mod 60
         let d = n % 60;
-        let ch: char = digits[d as usize] as char;
+
+        // Safe to index because 0 <= n < 60
+        // Safe to convert to char because the lookup table is all chars
+        let ch = DIGITS[d as usize] as char;
+
         s.push(ch);
         n = (n - d) / 60;
     }
     s.chars().rev().collect()
 }
 
+/// Convert a string into its New Base 60 representation, dropping invalid characters.
+///
+/// Valid New Base 60 characters are alphanumeric or underscores (that is, they
+/// individually match the regex `[a-zA-Z0-9_]`). Invalid characters will be treated as if
+/// they did not exist. Empty strings will evaluate to 0.
+///
+/// If the resulting value is larger than 2<sup>128</sup>, then this function will return `None`.
+///
+/// For more information, see [this link](http://tantek.pbworks.com/w/page/19402946/NewBase60).
+///
+/// # Examples
+///
+/// ```
+/// use newbase60_rs::sxg_to_num;
+///
+/// assert_eq!(sxg_to_num("NH"), Some(1337));
+/// assert_eq!(sxg_to_num("N🥺H"), Some(1337));
+/// assert_eq!(sxg_to_num("verylongstringthatoverflowsthemultiplicationbuffer"), None);
+/// ```
 pub fn sxg_to_num(s: &str) -> Option<u128> {
     let mut n: u128 = 0;
     for c in s.chars() {
@@ -34,6 +70,7 @@ pub fn sxg_to_num(s: &str) -> Option<u128> {
             'O' => 0,       // error correct typo capital O to 0
             _ => continue,  // skip invalid chars
         };
+
         n = match n.checked_mul(60).and_then(|x| x.checked_add(digit as u128)) {
             Some(x) => x,
             None => return None,
@@ -72,16 +109,16 @@ fn test_num_to_sxg(input: u128, expected: &str) {
 #[case("🥺", Some(0))]
 #[case("sadfui9fasjf", Some(1908097676891172549880))]
 #[case(
-"this is a very long string that will overflow the multiplication buffer",
-None
+    "this is a very long string that will overflow the multiplication buffer",
+    None
 )]
 fn test_sxg_to_num(input: &str, expected: Option<u128>) {
     assert_eq!(sxg_to_num(input), expected)
 }
 
-#[cfg(test)]
+#[test]
 fn test_round_trip_n_s_n() {
-    for n in 0..63832 as u128 {
+    for n in 0..100000 as u128 {
         let s = num_to_sxg(n);
         assert_eq!(sxg_to_num(s.as_str()), Some(n))
     }
